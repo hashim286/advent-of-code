fff8b31c6580d7bd1ac6a4e0576410e4e081716c
hashim_rauf <hashimrauf@gmail.com>
2024-06-11 21:33:16 -0400

    added part 1

---
+++ 1-trebuchet/part_one.py
@@ -0,0 +1,15 @@
+import re as re
+
+FILENAME = "input.txt"
+
+
+def main(file_name):
+    with open(file_name, "r") as f:
+        line_matches = list()
+        for line in f:
+            matches = re.findall(r"\d", line)
+            line_matches.append(int(matches[0] + matches[-1]))
+
+        print(sum(line_matches))
+
+main(FILENAME)
